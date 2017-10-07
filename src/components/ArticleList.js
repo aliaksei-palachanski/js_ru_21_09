cad752ec3ca9e2979a6f911959e390101072ea85
aliaksei-palachanski <daltondooling@gmail.com>
2017-10-07 12:05:02 +0300

    Implement HT4

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Accordion from './Accordion'
 import Article from './Article/index'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 
 class ArticleList extends Accordion {
     state = {
@@ -10,15 +10,21 @@ class ArticleList extends Accordion {
     }
 
     render() {
-        const { articles } = this.props
-        
-        if (this.state.error) return <h2>Error: {this.state.error.message}</h2>
-        if (!articles.length) return <h3>No Articles</h3>
+        const { articles, selectedDates, selectedArticles } = this.props;
+        let filteredArticles = articles.filter(a => (!selectedDates.from || new Date(a.date) > new Date(selectedDates.from)) &&
+            (!selectedDates.to || new Date(a.date) < new Date(selectedDates.to)));
 
-        const articleElements = articles.map((article) => <li key={article.id}>
+        if (!!selectedArticles && selectedArticles.length > 0){
+            filteredArticles = filteredArticles.filter(a => !!selectedArticles.filter(sa => sa.value === a.id)[0]);
+        }
+
+        if (this.state.error) return <h2>Error: {this.state.error.message}</h2>
+        if (!filteredArticles.length) return <h3>No Articles</h3>
+
+        const articleElements = filteredArticles.map((article) => <li key={article.id}>
             <Article article={article}
-                     isOpen={article.id === this.state.openItemId}
-                     onButtonClick={this.toggleOpenItemMemoized(article.id)}
+                isOpen={article.id === this.state.openItemId}
+                onButtonClick={this.toggleOpenItemMemoized(article.id)}
             />
         </li>)
         return (
@@ -44,5 +50,7 @@ ArticleList.propTypes = {
 }
 
 export default connect((state) => ({
-    articles: state.articles
+    articles: state.articles,
+    selectedDates: state.selectedDates,
+    selectedArticles: state.selectedArticles
 }))(ArticleList)
